0bedb1e6fdfd46b4d319e7776828a1d9632d67d4
Vivek Anand <va691187@gmail.com>
2026-04-25 22:50:19 +0530

    Mini Commit

--- src/utills/setting.py
+++ src/utills/setting.py
@@ -1,4 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
